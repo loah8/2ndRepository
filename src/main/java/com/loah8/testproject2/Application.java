6b04ebd3940690eb206e830c127b20a23e22d85d
byoree <maxby82@gmail.com>
2018-09-28 15:37:59 +0900

    2ndRepository test

--- src/main/java/com/loah8/testproject2/Application.java
+++ src/main/java/com/loah8/testproject2/Application.java
@@ -1,12 +1,9 @@
-package com.loah8.testproject;
+package com.loah8.testproject2;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication
-@EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class})
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class);
--- src/main/java/com/loah8/testproject2/controller/MainController.java
+++ src/main/java/com/loah8/testproject2/controller/MainController.java
@@ -1,4 +1,4 @@
-package com.loah8.testproject.controller;
+package com.loah8.testproject2.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
